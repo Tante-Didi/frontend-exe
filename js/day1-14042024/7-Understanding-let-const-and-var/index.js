5a76e4154bbaace3143bb705b35b9f1ce9a23658
TanteDidi <danielarmoni@yahoo.com>
2024-04-15 01:39:22 +0300

    7-Understanding-let-const-and-var

---
+++ js/day1-14042024/7-Understanding-let-const-and-var/index.js
@@ -0,0 +1,22 @@
+//declair var
+var num = 8;
+//declair let
+let dayInTheWeek = " Wednesday";
+//declair const
+const isItNowBadTime = "of-course";
+//Reassigning var
+var num2 = 10;
+console.log(num2); //10
+num2 = 20;
+console.log(num2); //20
+//Reassigning let
+let num5 = 50;
+console.log(num5); //50
+let num5 = 60;
+console.log(num5); //Identifier 'num5' has already been declared
+
+//Reassigning const
+const num7 = 70;
+console.log(num7); //70
+num7 = 90;
+console.log(num7); //TypeError: Assignment to constant variable.because you can`t override the declaration it is recommended to use const when you have a lot of code  .*/
