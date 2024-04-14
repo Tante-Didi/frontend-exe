5cbfcee4a9b8835c18de8a26251dbed0778f8c0a
TanteDidi <danielarmoni@yahoo.com>
2024-04-14 23:43:05 +0300

    5-String-concatenation

---
+++ js/day1-14042024/5-String-concatenation/index.js
@@ -0,0 +1,32 @@
+//Introduce Yourself
+const myName = "Daniela";
+const myAge = 40;
+console.log(" My name is " + myName + " and I am " + myAge + " years old "); //My name is Daniela and I am 40 years old
+// Favorite Things
+const myFavoriteColor = "Green";
+const myFavoriteMovie = "Wonder";
+const myFavoritefood = "Persian food";
+console.log(
+  " My favorite color is " +
+    myFavoriteColor +
+    " , " +
+    " I love the movie " +
+    myFavoriteMovie +
+    " , " +
+    " and I enjoy eating " +
+    myFavoritefood
+);
+//Simple Math
+const a = 4;
+const b = 6;
+const result = a + b;
+console.log(
+  "the sum of a and b is " +
+    result +
+    " were " +
+    result +
+    " is the sum of a and b"
+);
+//Day of the week
+const currentDayOfTheWeek = " Monday ";
+console.log(" today is " + currentDayOfTheWeek);
