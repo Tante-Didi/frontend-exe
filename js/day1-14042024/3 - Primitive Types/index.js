9fed07474b7537c976bbe6e504a2d68e25e60581
TanteDidi <danielarmoni@yahoo.com>
2024-04-14 22:34:20 +0300

    3-Primitive-Types

---
+++ js/day1-14042024/3 - Primitive Types/index.js
@@ -0,0 +1,21 @@
+//exploring string
+const firstName = " Daniela ";
+console.log(firstName);
+
+//playing with numbers
+const X = 5;
+const Y = 20;
+const sum = X + Y;
+console.log(sum);
+
+//boolean numbers
+const fiveCats = true;
+console.log(fiveCats);
+
+//undefined variables
+let calculator;
+console.log(calculator);
+
+//Null value
+let results = null;
+console.log(results);
