09d262bf6da0b5595c51e52df38ddf8a6afb2d72
TanteDidi <danielarmoni@yahoo.com>
2024-04-14 21:59:04 +0300

    2-type of Operator

---
+++ js/day1-14042024/2 - typeof Operator/index.js
@@ -0,0 +1,37 @@
+// type Identification
+const num = 10;
+console.log(typeof num); //number
+
+const str = "Hello";
+console.log(typeof str); //string
+
+const bool = true;
+console.log(typeof bool); //boolean
+
+// Comparing Variable Types
+const firstVar = 42;
+console.log(typeof firstVar); //number
+const secondVar = 42;
+console.log(typeof secondVar); //number
+
+//Undefined and Null Type
+let noValue;
+console.log(typeof noValue); //undefined
+let emptyValue = null;
+console.log(typeof emptyValue); //object
+
+//debugging Variables
+const number = 150;
+console.log(
+  " the value of num is " + number + " " + "and its type is " + typeof number
+);
+
+const string = "I love Ice Cream";
+console.log(
+  " the value of string is " + string + " " + "and its type is " + typeof string
+);
+
+const booli = false;
+console.log(
+  " the value of booli is " + booli + " " + "and its type is " + typeof booli
+);
