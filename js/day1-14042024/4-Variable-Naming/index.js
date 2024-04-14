85ce872bc6f17a625c0b8b8b042ed2f14e0249d2
TanteDidi <danielarmoni@yahoo.com>
2024-04-14 23:11:53 +0300

    4-Variable-Naming

---
+++ js/day1-14042024/4-Variable-Naming/index.js
@@ -0,0 +1,8 @@
+//city temperature for a weather app
+const parisCurrentTemperature = "30°C";
+// user login status for a website
+const isUserLoggedIn = "yes";
+//customer's email address
+const customerOnlineOderFormEmailAddress = "yaacov@gmail.com";
+//total score in a basketball Game
+const totalHapoelJerusalemBasketballGamesScore = 95;
