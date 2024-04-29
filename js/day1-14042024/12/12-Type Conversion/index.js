1b8e79cd77167d0f4a6608be1fe4a41c6ebe2315
TanteDidi <danielarmoni@yahoo.com>
2024-04-30 01:17:39 +0300

    Type Conversion

--- js/day1-14042024/12/12-Type Conversion/index.js
+++ js/day1-14042024/12/12-Type Conversion/index.js
@@ -1,21 +1,58 @@
 //1.convert number to string    .toString() method
+console.log("---------------1--------------");
 let number = 123;
 let numberToString = number.toString();
-console.log(`${numberToString}`);
+console.log(numberToString);
 //למה זה לא עובד???? למה זה לא נהפך לסטרינג?????
 
-//2. convert string to a number  parseInt()
+//2. convert string to a number
+console.log("---------------2--------------");
 let string = "456";
-let stringToNumber = parseInt("456");
+let stringToNumber = Number("456");
 console.log(stringToNumber);
 
 //3.convert string to float-point number    parseFloat()
+console.log("---------------3--------------");
 string = "78.9";
 stringToFloat = parseFloat(string);
 console.log(stringToFloat);
 
 //4.combining a number and string
+console.log("---------------4--------------");
 let number1 = 5;
 let string1 = "3";
 let sum = number1 + string1;
 console.log(sum);
+
+//5. Number to Boolean Conversion
+console.log("---------------5--------------");
+
+const numberZero = 0;
+const booleanZero = Boolean(numberZero);
+console.log(booleanZero); // false
+
+const numberOne = 1;
+const booleanOne = Boolean(numberOne);
+console.log(booleanOne); //  true
+
+//6.String to Number with a Unary Operator
+console.log("---------------6--------------");
+const string6 = "24";
+const number6 = +string6;
+console.log(number6);
+
+//7. True or False to Number:
+console.log("---------------7--------------");
+const trueBooleanSeven = true;
+const trueNumber = Number(trueBooleanSeven);
+console.log(trueNumber); // 1
+
+const falseBooleanSeven = false;
+const falseNumber = Number(falseBooleanSeven);
+console.log(falseNumber); //  0
+
+//8. Convert null to Number:
+console.log("---------------8--------------");
+const nullValue = null;
+const numberToNull = Number(nullValue);
+console.log(numberToNull); // 0
--- js/rpg-game-project/day5-21042024/15-String-methods/15-String-methods-exe.js
+++ js/rpg-game-project/day5-21042024/15-String-methods/15-String-methods-exe.js
@@ -1,12 +1,29 @@
 // 1. Character Name Capitalization
+console.log("--------------1-------------");
 function characterNameCapitalize(name) {
-  const string = name.split(",");
-  for (let i = 0; i < name.length; i++) {
-    const capitalize = string.touppercase(name[i][0]);
-    capitalize.join(" ");
+  let characterNameArray = name.split(" ");
+  let newString = "";
+  for (let i = 0; i < characterNameArray.length; i++) {
+    let capitalize =
+      characterNameArray[i].charAt(0).toUpperCase() +
+      characterNameArray[i].slice(1).toLowerCase();
+    newString = newString + capitalize + " ";
   }
+  return "" + newString + "";
 }
 
 const input = "iron warrior";
-characterNameCapitalize(name);
-console.log(input);
+const capitalizedInput = characterNameCapitalize(input);
+console.log(capitalizedInput); //Iron Warrior
+console.log("--------------2-------------");
+// 2. Item Search
+
+// Input: ["Iron Sword", "Healing Potion", "Steel Shield", "Iron Axe"] and query "Iron"
+console.log("--------------3-------------");
+console.log("--------------4-------------");
+console.log("--------------5-------------");
+console.log("--------------6-------------");
+console.log("--------------7-------------");
+console.log("--------------8-------------");
+console.log("--------------9-------------");
+console.log("--------------1-------------");
