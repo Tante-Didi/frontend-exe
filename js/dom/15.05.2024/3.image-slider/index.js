2d391f4e262dae06e8d0752de71cb375d705802f
TanteDidi <danielarmoni@yahoo.com>
2024-05-16 08:44:25 +0300

    exe- 3

--- js/dom/15.05.2024/3.image-slider/index.js
+++ js/dom/15.05.2024/3.image-slider/index.js
@@ -5,12 +5,12 @@ let currentIndex = 0;
 
 // //Moving Between Slides:
 const moveSlide = (direction) => {
-  slides[currentIndex].classList.remove("active");
+  slides[currentIndex].classList.remove("active"); //use the classList API to remove classes
   currentIndex += direction;
   if (currentIndex < 0) {
     currentIndex = 0;
   } else if (currentIndex > slides.length - 1) {
     currentIndex = slides.length - 1;
   }
-  slides[currentIndex].classList.add("active");
+  slides[currentIndex].classList.add("active"); //use the classList API to add classes
 };
